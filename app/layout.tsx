e96ca62684fe8e7a6b489199f52012920e5d8c56
Dave Garnar <dgarnar@gmail.com>
2026-03-06 11:56:15 +0000

    Add app icon: Imp on purple background for iPad home screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" href="/icons/imp.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
